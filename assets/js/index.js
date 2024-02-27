a0fad649f628641714b91c97eb341c1a79514bce
Navarroseb <sebastnavarro@gmail.com>
2024-02-27 17:53:08 -0300

    Generating card with stats

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,43 @@
+$(document).ready(function () {
+  let button = $('#button');
+  
+  button.click(function (e) {
+    e.preventDefault()
+    let superHeroNumber = $('#superHeroNumber').val()
+
+    if(superHeroNumber !== '' && !isNaN(superHeroNumber)){
+      let apiHero = 'https://www.superheroapi.com/api.php/10164257333809896/' + superHeroNumber;
+
+      $.ajax({
+        url: apiHero,
+        type: 'GET',
+        dataType: "json",
+        success: function(data) {
+          superHeroInfo(data);
+        },
+      });
+    } else {
+      alert('Por favor, ingrese un número válido para el superhéroe.');
+    }
+  });
+
+  const superHeroInfo = (heroData) =>{
+    let name = heroData.name;
+    let powerStats = heroData.powerstats;
+    let heroimage = heroData.image;
+    let card = $('#superHeroCard');
+
+    card.append(
+      
+    );
 
 
+
+  }
+ 
+  
+  
+
+
+
+})
